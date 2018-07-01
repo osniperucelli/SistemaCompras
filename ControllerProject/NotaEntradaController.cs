6c4a8f9081e0f6da5f41daa4a302a8ec96f9c7c9
osnipr@gmail.com <osnipr@gmail.com>
2018-07-01 17:53:54 -0300

    Implementado as classes NotaEntradaController e ProdutoController.

---
+++ ControllerProject/NotaEntradaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelProject;
+using PersistenceProject;
+
+namespace ControllerProject
+{
+    public class NotaEntradaController
+    {
+        private Repository repository = new Repository();
+
+        public NotaEntrada Insert(NotaEntrada notaEntrada) {
+            return this.repository.InsertNotaEntrada(notaEntrada);
+        }
+
+
+        public void Remove(NotaEntrada notaEntrada) {
+            this.repository.RemoveNotaEntrada(notaEntrada);
+        }
+
+
+        public IList<NotaEntrada> GetAllNotasEntrada() {
+            return this.repository.GetAllNotasEntrada();
+        }
+
+
+        public NotaEntrada Update(NotaEntrada notaEntrada) {
+            return this.repository.UpdateNotaEntrada(notaEntrada);
+        }
+
+        public NotaEntrada GetNotaEntradabyId(Guid Id) {
+            return this.repository.GetNotaEntradaById(Id);
+        }
+    }
+}
