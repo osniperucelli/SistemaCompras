e4dc24a2e786397c4b1de7a58a35fec283dfca18
osnipr@gmail.com <osnipr@gmail.com>
2018-07-01 20:01:41 -0300

    Subscrevendo metodos Equals e GetHashCode para que objeto possa ser buscado na selecao em fornecedor

--- ModelProject/Fornecedor.cs
+++ ModelProject/Fornecedor.cs
@@ -11,5 +11,25 @@ namespace ModelProject
         public Guid Id { get; set; }   //guid representa n inteiro de 16 bytes
         public string Nome { get; set; }
         public string CNPJ { get; set; }
+
+
+        //subscrevendo metodos Equals e GetHashCode para que objeto possa ser buscado na selecao em fornecedor.
+        protected bool Equals(Fornecedor other) {
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(Fornecedor))
+                return false;
+            return Equals((Fornecedor) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
